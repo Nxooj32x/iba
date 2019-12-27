59f7ec90317728e5a765c80334e8095826583a42
want <want@qq.com>
2019-12-27 13:45:46 +0800

    tune

--- lib/src/flut/framework/effect.dart
+++ lib/src/flut/framework/effect.dart
@@ -1,5 +1,6 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart' hide Action;
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:iba/src/flut/model/home/home_model.dart';
 import 'package:iba/src/flut/model/home/home_tab_model.dart';
 import 'action.dart';
@@ -37,7 +38,14 @@ void _initState(Action action, Context<BootState> ctx) {
   ctx.dispatch(BootActionCreator.onInitData([
     ctx.buildComponent('homeComponent'),
     Container(child: Center(child: Text("分类")),),
-    Container(child: Center(child: Text("社区")),),
+    Container(child: Center(child: IconButton(icon: Icon(Icons.settings_voice), onPressed: (){
+      var flutterTts = new FlutterTts();
+      flutterTts.setLanguage("en-US");
+      flutterTts.setSpeechRate(0.3);
+      flutterTts.setVolume(1.0);
+      flutterTts.setPitch(1.0);
+      flutterTts.speak('晚日寒鸦一片愁。柳塘新绿却温柔。若教眼底无离恨，不信人间有白头。肠已断，泪难收。相思重上小红楼。情知已被山遮断，频倚阑干不自由。');
+    })),),
     Container(child: Center(child: Text("收益")),),
     Container(child: Center(child: Text("我的")),),
   ],tickerProvider,appBarKey,homeModel));
